69fbd7a44f21bb3f25adb859a47c995631901e6d
Oleksii Bilan <alexey.belan@spryker.com>
2026-09-04 12:29:14 +0100

    CC-39944 Update Angular to v22 (#1111)
    CC-39944 Update Angular to v22

--- src/SprykerShop/Configurator/DateTimeConfiguratorPageExample/Theme/ConfiguratorApplication/src/app/app.config.ts
+++ src/SprykerShop/Configurator/DateTimeConfiguratorPageExample/Theme/ConfiguratorApplication/src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withXhr } from '@angular/common/http';
 import { TranslateLoader, provideTranslateService } from '@ngx-translate/core';
 import { APP_I18N_PREFIX, PROJECT_I18N_PREFIX } from '../services/configurator.service';
 import { MergedTranslateLoader } from '../services/merged-translate.loader';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideHttpClient(),
+        provideHttpClient(withXhr()),
         provideTranslateService({
             fallbackLang: 'en_US',
             loader: {
